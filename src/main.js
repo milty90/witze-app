baea0cc39934c3261706b7d2fe11091afaf6c688
milty90 <156122688+milty90@users.noreply.github.com>
2025-11-20 19:36:59 +0100

    refactor favorites functionality and update styles layout

--- src/main.js
+++ src/main.js
@@ -4,21 +4,30 @@ let favoriteJokes = [];
 const jokeButton = document.getElementById('jokeButton');
 const jokeDisplay = document.getElementById('jokeDisplay');
 const favoriteButton = document.getElementById('favoriteButton');
+const favoriteJokesList = document.getElementById('favoriteJokesList');
+
+initJokes();
 
 jokeButton.addEventListener('click', async () => {  
     const joke = await fetchJoke();
     jokeDisplay.textContent = joke;      
-}
-);
+});
 
 favoriteButton.addEventListener('click', () => {
-    loadJokes();
+    initJokes();
     const currentJoke = {jokeId: new Date().getTime(), jokeText: jokeDisplay.textContent};
     favoriteJokes.push(currentJoke);
     saveJokes(favoriteJokes);
     console.log("Witz gespeichert: " + currentJoke);
-    showFavoriteJokes();
+   
+
 });
+
+function initJokes() {
+    loadJokes();
+    showFavoriteJokes();
+}
+    
 
 function saveJokes(arrayJokes) {
     saveJokeToLocalStorage(JSON.stringify(arrayJokes));
@@ -31,7 +40,25 @@ function loadJokes() {
     }
 }
 
+function removeFavoriteJoke(jokeId) {
+    favoriteJokes = favoriteJokes.filter(joke => joke.jokeId !== jokeId);
+    saveJokes(favoriteJokes);
+    initJokes();
+}   
+
 function showFavoriteJokes() {
+    favoriteJokesList.innerHTML = '';
+    favoriteJokes.forEach(joke => {
+        const listItem = document.createElement('li');
+        listItem.className = 'joke-app__favorite';
+        listItem.innerHTML = `<p>${joke.jokeText}</p>
+        <img class="joke-app__button-icon" src="public/disable-favorite.svg" 
+        alt="Disable Favorite">`;
+        listItem.querySelector('img').addEventListener('click', () => {
+            removeFavoriteJoke(joke.jokeId);
+        });
+        favoriteJokesList.appendChild(listItem);
+    });
 
 }
 
